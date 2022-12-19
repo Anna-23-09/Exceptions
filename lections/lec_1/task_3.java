1c7cf0e2278a0e33bae099320d796f68a0f962f5
Anna <tolstovaaniuta@yandex.ru>
2022-12-19 14:06:31 +0300

    folders

---
+++ lections/lec_1/task_3.java
@@ -0,0 +1,14 @@
+package lec_1;
+
+public class task_3 {
+    public static void main(String[] args) {
+        divide(10, 0);
+    }
+
+    public static int divide (int a1, int a2) {
+        if (a2 == 0) {
+            throw new RuntimeException ("Divide by zero not permited.");
+        }
+        return a1 / a2;
+    }
+}
