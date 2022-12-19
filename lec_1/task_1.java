eb4500104b66000b3df61a108112e14928b65da3
Anna <tolstovaaniuta@yandex.ru>
2022-12-19 13:47:09 +0300

    job in lection

---
+++ lec_1/task_1.java
@@ -0,0 +1,23 @@
+package lec_1;
+import java.io.File;
+
+public class task_1 {
+    public static void main(String[] args) {
+        // System.out.println(getFileSize(new File("README.md"))); вызов функции getFileSize
+        // System.out.println(divide(10, 0)); вызов функции divide
+    }
+
+    public static int divide (int a1, int a2) {
+        if (a2 == 0) {
+            return -1;
+        }
+        return a1 / a2;
+    }
+
+    public static long getFileSize (File file) {
+        if (!file.exists()) {
+            return -1;
+        }
+        return file.length();
+    } 
+}
