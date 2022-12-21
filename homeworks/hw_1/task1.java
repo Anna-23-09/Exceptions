ed5cc8312b72490a0d046ac76ba60b90660d2532
Anna <tolstovaaniuta@yandex.ru>
2022-12-21 12:02:37 +0300

    1 task

--- homeworks/hw_1/task1.java
+++ homeworks/hw_1/task1.java
@@ -9,9 +9,9 @@ import javax.lang.model.util.SimpleAnnotationValueVisitor9;
 
 public class task1 {
     public static void main(String[] args) {
-        //System.out.println(dividing(5, 0));
-        //System.out.println(resulting(new int[] {1, 7, 9, 2}, 3));
-        System.out.println(checking_arr(new int[] {1, 0, 2, 0}));
+        System.out.println(dividing(5, 0));
+        System.out.println(resulting(new int[] {1, 7, 9, 2}, 3));
+        System.out.println(checking_arr(new int[] {1, 0, 0, 0, 2}));
         
     }
 
@@ -25,14 +25,16 @@ public class task1 {
         limit = arr.length;
         if (arr.length > limit - 1) {
             throw new RuntimeException("Incorrect limit");
-        } else return arr;
+        } 
+        else System.out.println(arr);
     }
 
     private static void checking_arr (int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
                 throw new RuntimeException("The array should only contain 0");
-            } else return arr;
+            } 
+            else System.out.println(arr);
         }
     }
 }
