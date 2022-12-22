3dc93fe7b7b77144fe5fa0a2439ce550483ea880
Anna <tolstovaaniuta@yandex.ru>
2022-12-22 13:10:20 +0300

    2 task

--- homeworks/hw_1/task1.java
+++ homeworks/hw_1/task1.java
@@ -9,8 +9,8 @@ import javax.lang.model.util.SimpleAnnotationValueVisitor9;
 
 public class task1 {
     public static void main(String[] args) {
-        System.out.println(dividing(5, 0));
-        System.out.println(resulting(new int[] {1, 7, 9, 2}, 3));
+        //System.out.println(dividing(5, 0));
+        //System.out.println(resulting(new int[] {1, 7, 9, 2}, 3));
         System.out.println(checking_arr(new int[] {1, 0, 0, 0, 2}));
         
     }
@@ -26,7 +26,7 @@ public class task1 {
         if (arr.length > limit - 1) {
             throw new RuntimeException("Incorrect limit");
         } 
-        else System.out.println(arr);
+        return arr;
     }
 
     private static void checking_arr (int[] arr) {
@@ -34,7 +34,7 @@ public class task1 {
             if (arr[i] != 0) {
                 throw new RuntimeException("The array should only contain 0");
             } 
-            else System.out.println(arr);
+            return arr;
         }
     }
 }
--- homeworks/hw_1/task2.java
+++ homeworks/hw_1/task2.java
@@ -4,6 +4,24 @@ package homeworks.hw_1;
 
 public class task2 {
     public static void main(String[] args) {
-        
+        System.out.println(sum2d(new String[][] {{"a", "b"}, {"b" , "a"}}));
+        System.out.println(wrong_length(new String[][] {{"a", "b"}, {"b", "a"}}));
+    }
+
+    public static int sum2d (String [][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                int val = Integer.parseInt(arr[i][j]);
+                sum += val;
+            }
+        }
+        return sum;
+    }
+
+    private static void wrong_length (String [][] arr) {
+        if (arr.length != arr[0].length) {
+            throw new RuntimeException("Массив неквадратный");
+        }
     }
 }
